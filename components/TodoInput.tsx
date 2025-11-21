3a804f01367ddef3bca34c59634760a09629e271
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-11-21 22:06:28 +0545

    Add TodoInput component for adding new todos with input handling and styling

---
+++ components/TodoInput.tsx
@@ -0,0 +1,55 @@
+import { createHomeStyles } from "@/assets/styles/home.style";
+import { api } from "@/convex/_generated/api";
+import { useTheme } from "@/hooks/useTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { useMutation } from "convex/react";
+import { LinearGradient } from "expo-linear-gradient";
+import React, { useState } from "react";
+import { TextInput, TouchableOpacity, View } from "react-native";
+
+const TodoInput = () => {
+  const { colors } = useTheme();
+
+  const homeStyles = createHomeStyles(colors);
+  const [todo, setTodo] = useState("");
+  const addTodo = useMutation(api.todos.addTodo);
+
+  async function addTodoHandler(e: any) {
+    addTodo({ title: e.nativeEvent.text });
+    setTodo("");
+  }
+  return (
+    <View style={homeStyles.inputSection}>
+      <View style={homeStyles.inputWrapper}>
+        <TextInput
+          style={homeStyles.input}
+          placeholder="Add a new todo"
+          placeholderTextColor={colors.textMuted}
+          value={todo}
+          onChangeText={setTodo}
+          onSubmitEditing={addTodoHandler}
+          //   multiline={true}
+        />
+        <TouchableOpacity
+          activeOpacity={0.8}
+          disabled={!todo.trim()}
+          onPress={addTodoHandler}
+        >
+          <LinearGradient
+            colors={
+              todo.trim() ? colors.gradients.primary : colors.gradients.muted
+            }
+            style={[
+              homeStyles.addButton,
+              !todo.trim() && homeStyles.addButtonDisabled,
+            ]}
+          >
+            <Ionicons name="add-outline" size={24} color={"#ffffff"} />
+          </LinearGradient>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default TodoInput;
