e3c1fb5dee3eaf75e6ae4ecc2f86a8db08e75457
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-11-21 22:05:15 +0545

    Add LoadingSpinner component for displaying a loading state

---
+++ components/LoadingSpinner.tsx
@@ -0,0 +1,25 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { ActivityIndicator, Text, View } from "react-native";
+
+import { useTheme } from "@/hooks/useTheme";
+import { createHomeStyles } from "../assets/styles/home.style";
+
+const LoadingSpinner = () => {
+  const { colors } = useTheme();
+
+  const homeStyles = createHomeStyles(colors);
+
+  return (
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={homeStyles.container}
+    >
+      <View style={homeStyles.loadingContainer}>
+        <ActivityIndicator size="large" color={colors.primary} />
+        <Text style={homeStyles.loadingText}>Loading your todos...</Text>
+      </View>
+    </LinearGradient>
+  );
+};
+
+export default LoadingSpinner;
