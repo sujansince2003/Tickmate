26073c1bc25cdd417b9a985cbf2585c2de3f4c10
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-11-22 16:23:13 +0545

    Refactor Settings component to improve structure and styling, incorporating new components for enhanced user experience

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,12 +1,37 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { createSettingsStyles } from "@/assets/styles/setting.style";
+import Clear from "@/components/Clear";
+import HeaderSettings from "@/components/HeaderSettings";
+import ProgressStats from "@/components/ProgressStats";
+import SettingsPreferences from "@/components/SettingsPreferences";
+import { useTheme } from "@/hooks/useTheme";
+import { LinearGradient } from "expo-linear-gradient";
+import { ScrollView, StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const settings = () => {
+const Settings = () => {
+  const { colors } = useTheme();
+  const settingStyles = createSettingsStyles(colors);
   return (
-    <View>
-      <Text>settings</Text>
-    </View>
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={settingStyles.container}
+    >
+      <StatusBar barStyle={colors.statusBarStyle} />
+
+      <SafeAreaView style={settingStyles.safeArea}>
+        <HeaderSettings settingStyles={settingStyles} />
+        <ScrollView
+          style={settingStyles.scrollView}
+          contentContainerStyle={settingStyles.content}
+          showsVerticalScrollIndicator={false}
+        >
+          <ProgressStats />
+          <SettingsPreferences />
+          <Clear />
+        </ScrollView>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
-export default settings;
+export default Settings;
